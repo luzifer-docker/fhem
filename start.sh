d19f86c27311050f5cb4bcd7383f92bc191202d6
Knut Ahlers <knut@ahlers.me>
2018-01-11 18:54:28 +0100

    Add support for custom modules
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- start.sh
+++ start.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-
-[ -e /data/config.cfg ] || cp /opt/fhem/fhem.cfg /data/config.cfg
+set -euo pipefail
 
 function finish {
   pgrep perl | xargs -r kill
 }
 trap finish EXIT
 
+# Initialize with default config if none is provided
+[ -e /data/config.cfg ] || cp /opt/fhem/fhem.cfg /data/config.cfg
+
+# Install custom modules if present
+[ -d /data/custom-modules ] && cp /data/custom-modules/* /opt/fhem/contrib/
+
+# Start the fhem daemon
 perl fhem.pl /data/config.cfg
 
+# Attach to logs
 tail -f ./log/*.log
